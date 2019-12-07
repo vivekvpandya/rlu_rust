e53bc9ffcef44d03aeabbce7f0867633b86f0d5c
ADVAIT <advaitsoman@gmail.com>
2019-12-07 13:24:44 +0530

    struggling eith [ointer cast

--- final/rlu/program/src/rlu.rs
+++ final/rlu/program/src/rlu.rs
@@ -349,7 +349,7 @@ fn rlu_reset_writer_locks(self_ : *mut rlu_thread_data_t, ws_id : usize) {
 fn rlu_reset_write_set(self_ : *mut rlu_thread_data_t, ws_counter : usize) {
     unsafe {
         // TODO: #define WS_INDEX(ws_counter) ((ws_counter) % RLU_MAX_WRITE_SETS)
-        let ws_id = 0; //Assuming single write set
+        let ws_id = WS_INDEX(ws_counter);
 
         (*self_).obj_write_set[ws_id].num_of_objs = 0;
         // TODO:Assigning har pointer to integer.. need to figure out better way
@@ -361,21 +361,21 @@ fn rlu_reset_write_set(self_ : *mut rlu_thread_data_t, ws_counter : usize) {
 
 //#define RLU_MAX_WRITE_SETS (200) // Minimum value is 2
 // #define WS_INDEX(ws_counter) ((ws_counter) % RLU_MAX_WRITE_SETS)
-fn WS_INDEX(ws_counter : u32) -> u32 {
-    ws_counter % 20;
+fn WS_INDEX(ws_counter : usize) -> usize {
+    ws_counter % 20
 }
 fn rlu_writeback_write_set(self_ : *mut rlu_thread_data_t, ws_counter: usize) {
 
         unsafe {
             /*TODO: need to think this routine, it has pointer manipulation
              *      can encapsulated in the functions*/ 
-            let ws_id = WS_INDEX(ws_counter);
-            //p_cur = (intptr_t *)&(self->obj_write_set[ws_id].buffer[0]);
+            let ws_id = WS_INDEX(ws_counter );
+            let p_cur =   &mut((*self_).obj_write_set[ws_id].buffer[0]);
+            
+         //   for i in  0..(*self_).obj_write_set[ws_id].num_of_objs {
+                let p_ws_obj_h : *mut rlu_ws_obj_header = p_cur as *mut rlu_ws_obj_header;
 
-            for i in  0..(*self_).obj_write_set[ws_id].num_of_objs {
-                p_ws_obj_h = (rlu_ws_obj_header_t *)p_cur;
-
-                p_obj_actual = (intptr_t *)p_ws_obj_h->p_obj_actual;
+               /* p_obj_actual = (intptr_t *)p_ws_obj_h->p_obj_actual;
                 obj_size = (obj_size_t)p_ws_obj_h->obj_size;
 
                 p_cur = MOVE_PTR_FORWARD(p_cur, WS_OBJ_HEADER_SIZE);
@@ -400,7 +400,7 @@ fn rlu_writeback_write_set(self_ : *mut rlu_thread_data_t, ws_counter: usize) {
 
                 UNLOCK(p_obj_actual);
                 }
-	//RLU_ASSERT(p_cur == self->obj_write_set[ws_id].p_cur);
+	//RLU_ASSERT(p_cur == self->obj_write_set[ws_id].p_cur);*/
 
     }
 }
